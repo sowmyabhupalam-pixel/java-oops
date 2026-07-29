4adcfea59376937d6186b7cb9a83adcec1641eba
Bhupalam Sai Sowmya <sowmyabhupalam@gmail.com>
2026-07-29 19:42:56 +0530

    Create SingleInheritance.java

---
+++ Inheritance/SingleInheritance.java
@@ -0,0 +1,12 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+    }
+}
