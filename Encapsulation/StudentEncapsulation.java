6bf75aba1f63645535ddcf62d64862130d53fe77
Bhupalam Sai Sowmya <sowmyabhupalam@gmail.com>
2026-07-29 12:15:45 +0530

    Create StudentEncapsulation.java

---
+++ Encapsulation/StudentEncapsulation.java
@@ -0,0 +1,17 @@
+class Student {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Sai Sowmya");
+        System.out.println("Name: " + s.getName());
+    }
+}
