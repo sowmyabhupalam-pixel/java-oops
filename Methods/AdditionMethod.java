5f9b6f078bd4fa64ff546b738556a8da8313fe4c
Bhupalam Sai Sowmya <sowmyabhupalam@gmail.com>
2026-07-29 11:50:11 +0530

    Create AdditionMethod.java

---
+++ Methods/AdditionMethod.java
@@ -0,0 +1,11 @@
+class AdditionMethod {
+    void add() {
+        int a = 10, b = 20;
+        System.out.println("Sum = " + (a + b));
+    }
+
+    public static void main(String[] args) {
+        AdditionMethod obj = new AdditionMethod();
+        obj.add();
+    }
+}
